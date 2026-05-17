7d860f15b47a20891f54f27fd75e18e9542d1b3a
danaiss <danaisslu@gmail.com>
2026-05-16 19:17:04 -0500

    cambios a paddington + botoncitos para bajar

---
+++ trapped-popups.js
@@ -0,0 +1,57 @@
+// =========================
+// SUBWAY
+// =========================
+
+function abrirSubwayPopup(){
+
+    document.getElementById("subwayPopup").style.display = "flex";
+
+    let tren = document.getElementById("subwayTrain");
+
+    tren.classList.remove("subway-move");
+
+    void tren.offsetWidth;
+
+    tren.classList.add("subway-move");
+}
+
+function cerrarSubwayPopup(){
+
+    document.getElementById("subwayPopup").style.display = "none";
+}
+
+// =========================
+// TIMES SQUARE
+// =========================
+
+function abrirTimesPopup(){
+
+    document.getElementById("timesPopup").style.display = "flex";
+
+    let flash = document.getElementById("timesFlash");
+
+    flash.classList.remove("times-square-active");
+
+    void flash.offsetWidth;
+
+    flash.classList.add("times-square-active");
+}
+
+function cerrarTimesPopup(){
+
+    document.getElementById("timesPopup").style.display = "none";
+}
+
+// =========================
+// CAMERA
+// =========================
+
+function abrirCameraPopup(){
+
+    document.getElementById("cameraPopup").style.display = "flex";
+}
+
+function cerrarCameraPopup(){
+
+    document.getElementById("cameraPopup").style.display = "none";
+}
